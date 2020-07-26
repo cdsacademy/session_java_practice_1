825cd582b65387f6e6ba841d4a5ca0663d695bd3
Carlos Caño Alegre <carlos.cano@hpcds.com>
2020-07-26 17:26:18 +0200

    Added method in repository

--- src/main/java/com/hpecds/academy/manager/dao/PersonRepository.java
+++ src/main/java/com/hpecds/academy/manager/dao/PersonRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.hpecds.academy.manager.model.Person;
 
-@RepositoryRestResource(exported=false)
+@RepositoryRestResource(exported=true)
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
     Person findByDni(@Param("dni") String dni);
+
+    Person findByNameAndSurname(@Param("alias") String name, @Param("secondName") String surname);
 
 }
